db11e266cb78e83731ea1c73c4a871a1c4a02216
EmersonSS <130619133+E-E-Silva@users.noreply.github.com>
2024-11-15 19:41:33 -0300

    :+1:
    GRANDE EVOLUÇÃO
    fazer o js e ajeitar as partes finais.

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
-var nome = window.prompt("Qual seu nome?")
-window.alert("É um grande prazer te conhecer," + nome + "!" );
-
-var numero = Number(window.prompt("Me INFORME UM NÚMERO"));
-var numero2 = Number(window.prompt("Me informe o segundo número"));
-var soma = numero += numero2;
-alert("A soma dos números informados é" + soma + ":D")
+// var nome = window.prompt("Qual seu nome?")
+// window.alert("É um grande prazer te conhecer," + nome + "!" );
+// var numero = Number(window.prompt("Me INFORME UM NÚMERO"));
+// var numero2 = Number(window.prompt("Me informe o segundo número"));
+// var soma = numero += numero2;
+// alert("A soma dos números informados é" + soma + ":D")
