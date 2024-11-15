15b0572fba5bf19165dd38b084ec1d8261ed2d55
EmersonSS <130619133+E-E-Silva@users.noreply.github.com>
2024-11-15 10:03:12 -0300

    wait

---
+++ main.js
@@ -0,0 +1,7 @@
+var nome = window.prompt("Qual seu nome?")
+window.alert("É um grande prazer te conhecer," + nome + "!" );
+
+var numero = Number(window.prompt("Me INFORME UM NÚMERO"));
+var numero2 = Number(window.prompt("Me informe o segundo número"));
+var soma = numero += numero2;
+alert("A soma dos números informados é" + soma + ":D")
